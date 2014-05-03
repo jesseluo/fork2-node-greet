6d6c8bb0c66c5c1d56b87cde171e6fc4045e21e3
Jesse Luo <radishchrist@gmail.com>
2014-05-04 01:54:19 +0800

    add warmup 1 files

---
+++ bin/greet.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+var parseArgs = require('minimist');
+var greetFunc = require("greet");
+
+var argv = parseArgs(process.argv.slice(2));
+
+// console.log(argv);
+console.log(greetFunc(argv._ ,argv.drunk));
