9c12e9da25f69f42e39f07751743996b3c9caf3b
Jesse Luo <radishchrist@gmail.com>
2014-05-05 01:37:06 +0800

    Lesson 2 finished

--- bin/greet.js
+++ bin/greet.js
@@ -6,4 +6,4 @@ var greetFunc = require("greet");
 var argv = parseArgs(process.argv.slice(2));
 
 // console.log(argv);
-console.log(greetFunc(argv._ ,argv.drunk));
+console.log(greetFunc(argv._, argv.drunk));
---
+++ test/greet_spec.js
@@ -0,0 +1,10 @@
+var greet = require("greet");
+
+describe('greet', function(){
+  it("should greet a person by name", function(){
+    expect(greet("jesse")).to.eql("hello, jesse");
+  });
+  it("should greet a person flirtatiously if drunk", function(){
+    expect(greet("jesse", true)).to.eql("hello jesse, you look sexy today");
+  });
+});
